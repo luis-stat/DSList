7ca60949e60f56b1c84df6b7a4a63cc21a110718
Luís Miguel <luismiguelpessoal1@gmail.com>
2025-08-19 16:12:26 -0300

    GET game by id

--- src/main/java/com/luistat/dslist/dto/GameDTO.java
+++ src/main/java/com/luistat/dslist/dto/GameDTO.java
@@ -1,50 +1,27 @@
-package com.luistat.dslist.entities;
+package com.luistat.dslist.dto;
 
-import java.util.Objects;
+import org.springframework.beans.BeanUtils;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.luistat.dslist.entities.Game;
 
-@Entity
-@Table(name = "tb_game")
-public class Game {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+public class GameDTO {
+
 	private Long id;
 	private String title;
-	
-	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
 	private String platforms;
 	private Double score;
 	private String imgUrl;
-	
-	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
-	
-	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
-	public Game() {
+	public GameDTO() {
+		
 	}
-
-	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
-			String shortDescription, String longDescription) {
-		this.id = id;
-		this.title = title;
-		this.year = year;
-		this.genre = genre;
-		this.platforms = platforms;
-		this.score = score;
-		this.imgUrl = imgUrl;
-		this.shortDescription = shortDescription;
-		this.longDescription = longDescription;
+	
+	public GameDTO(Game entity) {
+		BeanUtils.copyProperties(entity, this);
 	}
 
 	public Long getId() {
@@ -83,14 +60,14 @@ public class Game {
 		return platforms;
 	}
 
-	public void setPlatform(String platforms) {
+	public void setPlatforms(String platforms) {
 		this.platforms = platforms;
 	}
-	
+
 	public Double getScore() {
 		return score;
 	}
-	
+
 	public void setScore(Double score) {
 		this.score = score;
 	}
@@ -117,23 +94,6 @@ public class Game {
 
 	public void setLongDescription(String longDescription) {
 		this.longDescription = longDescription;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Game other = (Game) obj;
-		return Objects.equals(id, other.id);
 	}
 	
 }
